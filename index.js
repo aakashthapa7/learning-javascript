55994026a2d4312d91d868c02341bb4c96637515
aakashthapa1 <thpaakash99@gmail.com>
2026-09-24 12:07:18 +0545

    slice and dice

--- index.js
+++ index.js
@@ -21,5 +21,12 @@ function test2() {
   var textLength = text.length;
   alert("You have written " + textLength + " characters." + " Now you have " + (280 - text.length) + " characters left.");
 
+  // Reducing the text to fit within the character limit
+  var text2 = prompt("Enter some text to tweet:");
+  var cuttext2 = text2.slice(0, 280);
+  alert("Your tweet is: " + cuttext2);
+  // alternate method to reduce the text to fit within the character limit
+  alert("Your tweet is: " + prompt("Enter some text to tweet:").slice(0, 280)); 
+
 }
 
