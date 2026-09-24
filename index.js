30ed2edab666b145c072fc4cdd744b64ffd0c1ff
aakashthapa1 <thpaakash99@gmail.com>
2026-09-24 14:52:09 +0545

    changing character case

--- index.js
+++ index.js
@@ -28,5 +28,23 @@ function test2() {
   // alternate method to reduce the text to fit within the character limit
   alert("Your tweet is: " + prompt("Enter some text to tweet:").slice(0, 280)); 
 
+  //Changind case of the text using toUpperCase() and toLowerCase()
+  var text3 = prompt("Enter your name: ");
+  var len = text3.length;
+  var first = text3.slice(0, 1);
+  var rest = text3.slice(1, len);
+  alert("Hello: " + first.toUpperCase() + rest.toLowerCase());
+
 }
 
+// function test3() {
+//    //Changind case of the text using toUpperCase() and toLowerCase() but only using the console.log() method
+//   var text3 = console.log("Enter your name: ");
+//   var len = text3.length;
+//   var first = text3.slice(0, 1);
+//   var rest = text3.slice(1, len);
+//   console.log("Hello: " + first.toUpperCase() + rest.toLowerCase());
+// }
+// test3();
+
+test1();
